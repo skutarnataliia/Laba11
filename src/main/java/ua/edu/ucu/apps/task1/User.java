0dca92633d8c54c8315f10c2e614949f80eb9345
Nata <skutar.pn@ucu.edu.ua>
2023-12-26 20:26:09 +0200

    task 1 update

--- src/main/java/ua/edu/ucu/apps/task1/User.java
+++ src/main/java/ua/edu/ucu/apps/task1/User.java
@@ -1,16 +1,13 @@
 package ua.edu.ucu.apps.task1;
 
-import java.util.List;
-
-import lombok.Builder;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
-@Builder @ToString
+@SuperBuilder @ToString
 public class User {
     private String name;
     private int age;
     private String gender;
     private double weight;
     private double height;
-    private List<Integer> grades;
 }
